7e46b7ce41254bb16d72251a8ea9c2e1bc01312f
Gonzalo Zevallos <gonzalo.zev@gmail.com>
2020-11-15 20:54:26 -0300

    falta cli

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const fs = require('fs');
 
-const action = process.argv[1];
+const action = process.argv[2];
 
 switch (action) {
-  case 'init':
-    fs.copyFileSync(path.resolve(__dirname, './database'), process.cwd());
+  case 'init':   
+    const src = path.resolve(__dirname, "./database/hola.js");
+    const destination = process.argv[3] ? path.resolve(__dirname, process.argv[3]) : process.cwd();
+
+    fs.copyFileSync(src, destination);
+
     break;
 
   default:
