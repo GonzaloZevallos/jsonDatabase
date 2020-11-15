7d0d4356b6af3eb817cbe5d3d562b64ce10d65af
Gonzalo Zevallos <gonzalo.zev@gmail.com>
2020-11-15 15:20:04 -0300

    add init

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
-module.exports = require('./database/Models');
+const path = require('path');
+const fs = require('fs');
+
+const action = process.argv[1];
+
+switch (action) {
+  case 'init':
+    fs.copyFileSync(path.resolve(__dirname, './database'), process.cwd());
+    break;
+
+  default:
+    console.log('Command not recognized.')
+    break;
+}
