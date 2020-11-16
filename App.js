ba0afa28c3c1f8dfc77757c70d3c434ccc767051
Gonzalo Zevallos <gonzalo.zev@gmail.com>
2020-11-15 22:04:31 -0300

    add mkdir and copy dir

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
 const path = require('path');
+const copyDir = require('copy-dir');
 const fs = require('fs');
 
 const action = process.argv[2];
 
 switch (action) {
   case 'init':   
-    const src = path.resolve(__dirname, "./database/hola.js");
-    const destination = process.argv[3] ? path.resolve(__dirname, process.argv[3]) : process.cwd();
+    const src = path.resolve(__dirname, "./database");
+    const destination = path.resolve(process.argv[3] ? path.resolve(__dirname, process.argv[3]) : process.cwd(), './database');
 
-    fs.copyFileSync(src, destination);
+    fs.mkdirSync(destination);
 
+    copyDir(
+      src,
+      destination,
+      {
+        utimes: true,
+        mode: true,
+        cover: true,
+      }
+    );
+
+    console.log(`
+    
+    Structure created successfully:
+
+    database
+        |__ Class
+        |__ data
+        |__ Models
+    
+    `);
     break;
 
   default:
