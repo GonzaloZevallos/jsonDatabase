e98e591ea55898f3647656bd0be442254a350052
Gonzalo Zevallos <gonzalo.zev@gmail.com>
2020-11-15 22:27:39 -0300

    fix cli

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // ./cli.js
 let app = require('./app.js')
-const action = process.argv[1];
-const dest = process.argv[2];
+const action = process.argv[2];
+const dest = process.argv[3];
 app(action, dest)
